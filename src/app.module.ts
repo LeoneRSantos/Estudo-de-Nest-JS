e2b90f76391af6441645587da8a2126047546bff
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-06 09:39:56 -0300

    Refatoração de app.module com a adição do módulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsuariosControllerController } from './usuarios.controller/usuarios.controller.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [UsuariosControllerController],
 })
 export class AppModule { }
