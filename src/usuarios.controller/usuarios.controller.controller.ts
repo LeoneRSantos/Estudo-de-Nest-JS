aa5e5cb6e12a8945fc5caa99c6b1f879fb6ae391
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-06 09:51:43 -0300

    Refatoração do controller de usuários

--- src/usuarios.controller/usuarios.controller.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('usuarios')
-export class UsuariosControllerController {
-    @Get('lista-de-usuarios')
-    listarUsuarios() {
-        return 'Lista de usuários';
-    }
-}
