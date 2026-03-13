657ad983c3f791b5cde0f0d8dec965f163714d72
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-12 23:05:30 -0300

    Refatoração de imports em App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
+import { DatabaseModule } from './database/database.module';
+import { PostModule } from './post/post.module';
+import { AppController } from './app/app.controller';
+import { UsersService } from './user/user.service';
+import { PostsService } from './post/post.service';
 
 @Module({
   imports: [AuthModule, UserModule],
