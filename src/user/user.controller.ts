0fe7e5a47bbe7a187eb1d765a00bc80732a306bc
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-14 15:32:27 -0300

    Criação de método Get para teste do bcrypt em user.controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,16 @@ import * as bcrypt from 'bcrypt';
 
 @Controller('usuario')
 export class UserController {
+
+    @Get('criptografia-teste')
+    async encpritacaoTeste(@Body() dado: { senha: string },): Promise<any> {
+        const { senha } = dado;
+        const hash = await bcrypt.hash(senha, 10);
+        const evalida = await bcrypt.compare(senha, hash);
+        return { senha, hash, evalida };
+    }
+
+
     @Get('lista-de-usuarios')
     listarUsuarios(): String {
         return 'Lista de usuários';
