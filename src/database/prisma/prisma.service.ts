44ad3e5ba84b3a8eb169c0a7a4e70b0435ea4d4b
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-17 16:15:59 -0300

    Refatoração em Prisma Service

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,12 +1,16 @@
-
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    constructor(configService: ConfigService) {
+        const url = configService.get<string>('DATABASE_URL');
+
+        const adapter = new PrismaBetterSqlite3({ url });
+        console.log("Veja:", url);
+
         super({ adapter });
     }
 }
