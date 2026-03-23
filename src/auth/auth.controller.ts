a63cadf7e8980a96b988b9875e34b23ef3168f1e
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-23 11:48:22 -0300

    Atualização dos imports de auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import type { UserLoginInput } from 'src/generated/prisma/models';
 
 @Controller('auth')
 export class AuthController {}
