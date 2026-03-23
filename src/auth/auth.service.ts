3c7ccb17004f260a6a98d253db74de18200ccbc6
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-23 12:13:49 -0300

    Atualização dos imports de auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import type { UserLoginInput } from 'src/generated/prisma/models';
 
 @Injectable()
 export class AuthService {}
