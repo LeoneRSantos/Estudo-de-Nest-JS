56a8b78084ca319d9a6e319cdb780e3083bead8e
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-23 12:15:28 -0300

    Adição de PrismaService no construtor de AuthService e criação do método login() na classe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,4 +4,32 @@ import * as bcrypt from 'bcrypt';
 import type { UserLoginInput } from 'src/generated/prisma/models';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async login(dados: UserLoginInput): Promise<{ message: string } | { token: string }> {
+        const usuario = await this.prisma.user.findUnique({
+            where: { email: dados.email },
+        });
+        if (!usuario) {
+            return { message: "Usuário não encontrado" };
+        }
+        const senhaHash = await bcrypt.compare(dados.password, usuario.password);
+
+        if (senhaHash) {
+            const senhaValida = await this.prisma.user.findFirst({
+                where: { email: dados.email, password: usuario.password },
+            });
+
+            return { token: `Usuário ${senhaValida?.name} autenticado com sucesso! \n${JSON.stringify(senhaValida)}` };
+
+        }
+
+        if (!senhaHash) {
+            return { message: "Senha inválida" };
+        }
+
+        return { token: "Usuário autenticado com sucesso." };
+
+    }
+}
