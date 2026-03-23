c61a62c8f9204bcde625fef0bfe8b1c3c551ea31
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-23 11:46:20 -0300

    Atualização de imports de auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/user/user.service';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Module({
   controllers: [AuthController],
