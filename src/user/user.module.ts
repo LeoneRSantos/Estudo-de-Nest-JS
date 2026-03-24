e08da50afa5238d33896f8849b5a6364e5db80b9
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-24 11:02:33 -0300

    Atualização de imports e exports em user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule } from 'src/database/database.module';
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UserModule { }
