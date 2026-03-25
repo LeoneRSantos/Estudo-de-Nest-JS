ad60ccd5083e29895f5fef1bcf663a577dd4e780
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-25 11:27:23 -0300

    Adição de mensagem indicativa de inexistência do token em auth.guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
         if (!token) {
-            throw new UnauthorizedException();
+            throw new UnauthorizedException('Usuário não autenticado');
         }
         try {
             const payload = await this.jwtService.verifyAsync(token);
