beca9c6ee526f13298915c36d2450ea6c5e1f571
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-03-28 19:30:10 -0300

    Criação da classe LocalStrategy com método validade

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,27 @@
+
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService, private prisma: PrismaService) {
+        super();
+    }
+
+
+    async validate(dados: { email: string, password: string }): Promise<{ message: string }> {
+
+        const usuario = await this.prisma.user.findUnique({
+            where: { email: dados.email },
+        });
+
+        if (!usuario) {
+            return { message: "Usuário não encontrado" };
+        }
+
+        return { message: `Login do usuário ${usuario.name}` };
+    }
+}
