188fbcee4a2b2ae8226566eee66b4b6de6207584
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-04-01 22:08:36 -0300

    Criação da classe Prisma Mocks

---
+++ test/mocks/prisma.mock.ts
@@ -0,0 +1,14 @@
+export const prismaMock = {
+    user: {
+        findMany: jest.fn(),
+        findUnique: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+    },
+
+    post: {
+        findMany: jest.fn(),
+        create: jest.fn(),
+    },
+};
