ded05eae62c16b326c88888693cb6296265dbb59
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-04-13 11:38:22 -0300

    Adição de imports em App.controller

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { UsersService } from '../user/user.service';
+import { PrismaService } from '../database/prisma/prisma.service';
+import { PostsService } from '../post/post.service';
 
 describe('AppController', () => {
   let controller: AppController;
