e3abcf275019d2a299ee797110b88a41e1bf3887
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-04-20 13:57:35 -0300

    Refatoração: teste para retornar um email de outro usuário em user.service.spec

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -46,7 +46,7 @@ describe('UserService', () => {
     expect(resultado).toEqual(false);
   });
 
-  it('Deve retornar mensagem para e-mail existente de outro usuário', async () => {
+  it('Deve retornar um e-mail existente de outro usuário', async () => {
     const mockUsers = [
       { id: 1, email: 'usuario@emailvalido.com', name: 'Usuário Teste', password: 'senhadousuarioteste' },
     ];
