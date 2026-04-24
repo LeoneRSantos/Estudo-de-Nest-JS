2d80bfad923cee8f3ddca211978878b0411caa9a
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-04-24 13:42:05 -0300

    Refatoração: criação de usuários mock em usuarios-helpers

--- test/mocks/usuarios-helpers.ts
+++ test/mocks/usuarios-helpers.ts
@@ -2,4 +2,16 @@
 
 export const usuariosMock = [
     { id: 1, email: 'usuario@emailvalido.com', name: 'Usuário Teste', password: 'senhadousuarioteste' },
-]; 
+];
+
+export const usuarioMock = {
+    id: 1,
+    email: 'usuario@emailvalido.com',
+    name: 'Usuário Teste',
+    password: 'senhadousuarioteste',
+};
+
+export const usuarioInexistente = {
+    email: 'usuarioInexistente@email.com',
+    password: 'senhainexistente'
+}
