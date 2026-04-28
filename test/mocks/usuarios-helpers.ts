0fb47710ae51b14caafb46a8436c7795c7f9ffb7
LeoneRSantos <leonnerodriguez1@gmail.com>
2026-04-28 14:06:09 -0300

    adição de usuariosMock em usuarios-helpers

--- test/mocks/usuarios-helpers.ts
+++ test/mocks/usuarios-helpers.ts
@@ -1,7 +1,36 @@
 // Mock de usuários para teste
 
 export const usuariosMock = [
-    { id: 1, email: 'usuario@emailvalido.com', name: 'Usuário Teste', password: 'senhadousuarioteste' },
+    {
+        id: 1,
+        email: 'usuario@emailvalido.com',
+        name: 'Usuário Teste',
+        password: 'senhadousuarioteste'
+    },
+    {
+        id: 2,
+        email: 'usuario2@emailvalido.com',
+        name: 'Usuário Teste2',
+        password: 'senhadousuarioteste2'
+    },
+    {
+        id: 3,
+        email: 'usuario3@emailvalido.com',
+        name: 'Usuário Teste3',
+        password: 'senhadousuarioteste'
+    },
+    {
+        id: 4,
+        email: 'usuario4@emailvalido.com',
+        name: 'Usuário Teste4',
+        password: 'senhadousuarioteste'
+    },
+    {
+        id: 5,
+        email: 'usuario5@emailvalido.com',
+        name: 'Usuário Teste5',
+        password: 'senhadousuarioteste'
+    }
 ];
 
 export const usuarioMock = {
